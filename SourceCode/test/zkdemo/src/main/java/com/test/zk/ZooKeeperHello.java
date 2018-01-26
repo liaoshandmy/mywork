daf4e5b52ca66d4e22309e1ec80bf2839dde2329
liaoshandmy <liaoshandmy@126.com>
2018-01-26 21:45:18 +0800

    Zookeeper test.

--- SourceCode/test/zkdemo/src/main/java/com/test/zk/ZooKeeperHello.java
+++ SourceCode/test/zkdemo/src/main/java/com/test/zk/ZooKeeperHello.java
@@ -2,19 +2,31 @@ package com.test.zk;
 
 import java.io.IOException;
 
-import org.apache.zookeeper.CreateMode;
+import org.I0Itec.zkclient.ZkClient;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.data.Stat;
 
 public class ZooKeeperHello {
 
 	public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-		ZooKeeper zk = new ZooKeeper("192.168.213.125:2181", 300000, new DemoWatcher());//连接zk server
-        String node = "/app1";
+		//ZooKeeper zk = new ZooKeeper("192.168.213.125:2181", 300000, new DemoWatcher());//连接zk server
+		/*
+		ZooKeeper zk = new ZooKeeper("192.168.213.125:2181,192.168.213.125:2182,192.168.213.125:2183", 300000, new DemoWatcher());//连接zk server
+		if (!zk.getState().equals(ZooKeeper.States.CONNECTED)) {
+		    while (true) {
+				if (zk.getState().equals(ZooKeeper.States.CONNECTED)) {
+				   break;
+				}
+				try {
+				   TimeUnit.SECONDS.sleep(5);
+				} catch (InterruptedException e) {
+				   e.printStackTrace();
+				}
+			}
+		}
+		
+		String node = "/app1";
         Stat stat = zk.exists(node, false);//检测/app1是否存在
         if (stat == null) {
             //创建节点
@@ -25,6 +37,14 @@ public class ZooKeeperHello {
         byte[] b = zk.getData(node, false, stat);
         System.out.println(new String(b));
         zk.close();
+        */
+		
+		ZkClient zkClient = new ZkClient("192.168.213.125:2181,192.168.213.125:2182,192.168.213.125:2183");
+        String node = "/app2";
+        if (!zkClient.exists(node)) {
+            zkClient.createPersistent(node, "hello zk");
+        }
+        System.out.println(zkClient.readData(node));
 	}
 	
 	
