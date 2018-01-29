af47ca680659cb31887709b68bee98a8d530d968
liaoshandmy <liaoshandmy@126.com>
2018-01-29 19:59:15 +0800

    Test dubbo about helloworld.

---
+++ SourceCode/test/dubbo-server/src/main/java/main/MyServerMain.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyServerMain {
+
+	public static void main(String[] args) throws IOException {
+
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( new String[] { "applicationProvider.xml" });
+		context.start();
+
+		System.out.println("输入任意按键退出 ~ ");
+		System.in.read();
+		context.close();
+	}
+}
